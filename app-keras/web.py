958969a75af30140f7caa113ba6b3f270230aea6
andrewlee2015@u.northwestern.edu <andrewlee2015@u.northwestern.edu>
2020-06-09 15:20:26 -0700

    CI/CD config

--- app-keras/web.py
+++ app-keras/web.py
@@ -199,4 +199,4 @@ def prediction_payload(access):
 if __name__ == "__main__":
     from waitress import serve
 
-    serve(app, host="0.0.0.0", port="8080")
+    serve(app, host="0.0.0.0", port="80")
